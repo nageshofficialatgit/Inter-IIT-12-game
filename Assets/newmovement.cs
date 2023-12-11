a0437c9494efa26cc1b8ed0cc4d5128817d8b36b
Vaibhav <vg899050@gmail.com>
2023-12-11 09:44:14 +0530

    Initial Commit

---
+++ Assets/newmovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newmovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
